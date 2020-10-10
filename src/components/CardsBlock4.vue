79a6c7aa2b77bab697136f4b4f925c720551ee67
ssbsweet <ssbsweet56@gmail.com>
2020-10-10 14:48:53 +0300

    ver. 2.0.2

--- src/components/CardsBlock4.vue
+++ src/components/CardsBlock4.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col s12 m7 offset-m1">
     <div class="card-panel grey lighten-5 z-depth-3">
-      <a href="https://github.com/ssbsweet">https://github.com/ssbsweet</a>
+      <a href="https://github.com/ssbsweet/resume-site-dev">Сайт-резюме</a>
     </div>
   </div>
 </template>
